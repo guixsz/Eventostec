1afd706791eb8bc286e500f0af0de7c6f067c3b8
Guilherme Santana <guiguilsantana@gmail.com>
2024-07-01 01:20:36 -0300

    refactor: Creating EventController and web requisition createEvent

---
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -0,0 +1,33 @@
+package com.eventostec.api.controller;
+
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.service.EventService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+
+    private EventService eventService;
+
+    public EventController(EventService eventService){
+        this.eventService = eventService;
+    }
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Event> create(@RequestParam("title") String title,
+                                        @RequestParam(value = "description", required = false) String description,
+                                        @RequestParam("date") Long date,
+                                        @RequestParam("city") String city,
+                                        @RequestParam("state") String state,
+                                        @RequestParam("remote") Boolean remote,
+                                        @RequestParam("eventUrl") String eventUrl,
+                                        @RequestParam(value = "image", required = false)MultipartFile image){
+        EventRequestDTO eventRecordDTO = new EventRequestDTO(title, description, date, city, state, remote, eventUrl, image);
+        Event newEvent = this.eventService.createEvent(eventRecordDTO);
+        return ResponseEntity.ok(newEvent);
+    }
+}
